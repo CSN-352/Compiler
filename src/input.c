c6c140bcdbfd156fe4317d0c414db8c31529d90c
Siya Arora <arorasiya008@gmail.com>
2025-04-06 11:13:20 +0530

    fix string literal 3ac

--- src/input.c
+++ src/input.c
@@ -1,19 +1,3 @@
-// int func(int x){
-//     int y = 1;
-    
-//     return 0;
-// }
-
-
-// int main(){
-//     int x = 1;
-//     int y = 2;
-//     func(x);
-//     return 2;
-// }
-
-int func(int x){
-    int a = 1+2;
-    return a+2;
+int main(){
+    char* x = "hello world";
 }
-
