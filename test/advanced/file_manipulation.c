e05cc7b9c284c6e5baef87d0f7bd1e24b808eec4
demigod <divyanshjain.2206@gmail.com>
2025-04-06 23:34:22 +0530

    Pass file_manipulation and inheritance test

--- test/advanced/file_manipulation.c
+++ test/advanced/file_manipulation.c
@@ -1,4 +1,6 @@
-struct iobuf
+// Passing
+
+typedef struct iobuf
 {
     char*   _ptr;
     int _cnt;
@@ -9,8 +11,22 @@ struct iobuf
     int _bufsiz;
     char*   _tmpfname;
 } FILE;
+
+FILE* fopen(const char* filename, const char* mode){
+    return (FILE*)0x12345678; // Dummy file pointer for example
+}
+int fclose(FILE* stream){
+    return 0; // Dummy return value for example
+}
+void fprintf(FILE* stream, const char* format, ...){}
+void rewind(FILE* stream){}
+void printf(const char* format, ...){}
+char* fgets(char* str, int num, FILE* stream){
+    return str; // Dummy implementation for example
+}
+
 int main() {
-    struct FILE *f = fopen("test.txt", "w+"); fprintf(f, "Hello, File!"); rewind(f);
+    FILE *f = fopen("test.txt", "w+"); fprintf(f, "Hello, File!"); rewind(f);
     char buf[20]; fgets(buf, 20, f); printf("%s", buf);
     fclose(f);
 }
--- test/advanced/inheritance.c
+++ test/advanced/inheritance.c
@@ -1,14 +1,67 @@
-class Basic_Class {
-    private{
-        int a;
-    }
-    public{
-        int x;
+// Passing
+
+class A1 {
+public:
+    int x1;
+protected:
+    int y1;
+private:
+    int z1;
+};
+
+class A2 {
+public:
+    int x2;
+protected:
+    int y2;
+private:
+    int z2;
+};
+
+class A3 {
+public:
+    int x3;
+protected:
+    int y3;
+private:
+    int z3;
+    int inherited_from_A3() {
+        return 0;
     }
 };
 
-class B <- A, C {
-    public{
-        int x;
+// class B {
+class B <- public A1, protected A2, private A3 {
+public:
+    int x4;
+    int func(int a, int b) {
+        return a + b;
     }
+protected:
+    int y4;
+    int protected_func() {
+        return 0;
+    }
+private:
+    int z4;
 };
+
+
+int main() {
+    class B obj;
+    int x;
+    // breakpoint_global;
+    int sum = obj.inherited_from_A3(1, 2);
+    x = obj.x1;
+    // x = obj.y1; // Error: y1 is protected in A1
+    // x = obj.z1; // Error: z1 is private in A1
+    // x = obj.x2; // Error: x2 is protected in A2
+    // x = obj.y2; // Error: y2 is protected in A2
+    // x = obj.z2; // Error: z2 is private in A2
+    // x = obj.x3; // Error: x3 is private in A3
+    // x = obj.y3; // Error: y3 is private in A3
+    // x = obj.z3; // Error: z3 is private in A3
+    x = obj.x4;
+    // x = obj.y4; // Error: y4 is protected in B
+    // x = obj.z4; // Error: z4 is private in B
+}
