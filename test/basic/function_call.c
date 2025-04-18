52ed66796a6dbebfa0aee04716681e2ead862e77
sukhman <sukhmansinghsaluja@gmail.com>
2025-04-18 06:04:35 +0530

    Fixed function overloading

--- test/basic/function_call.c
+++ test/basic/function_call.c
@@ -6,24 +6,21 @@ int add(int a, int b)
 }
 void add(int a, char b)
 {
+    // breakpoint;
     // printf("%d", a);
     b = 'a';
     return;
     // add(a,b+1);
     // return 100;
 }
-void add()
-{
+void add3(){
+    return;
 }
-
-int main()
-{
+int main(){
     int sum = add(5, 10);
     // breakpoint;
-    // {
-    //     breakpoint_global;
-    // }
-    // int k = add(5, 'a');
-    // add(5, 'a');
-    // add();
+    // breakpoint_global;
+    add(5, 'a');
+    // add2(5, 'a');
+    // add3();
 }
