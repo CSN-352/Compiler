41cc7212edd4984709aeeeab99bdb26821598350
Aaditya Aren <omaren0248@gmail.com>
2025-04-22 23:54:50 +0530

    Implement function params, return and calls

--- src/codegen.h
+++ src/codegen.h
@@ -25,7 +25,8 @@ enum MIPSRegister {
     S0, S1, S2, S3, S4, S5, S6, S7,
     GP, SP, FP, RA , HI, LO, 
     // Floating Point Registers
-    F0, F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12, F13, F14, F15, F16, F17, F18, F19, F20, F21, F22, F23, F24, F25, F26, F27, F28, F29, F30, F31
+    F0, F1, // reserved for return values
+    F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12, F13, F14, F15, F16, F17, F18, F19, F20, F21, F22, F23, F24, F25, F26, F27, F28, F29, F30, F31
 };
 
 std::string get_mips_register_name(MIPSRegister reg);
