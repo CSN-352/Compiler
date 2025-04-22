715de7fc2b6857bd3dbf49d2f549194a30a98131
sukhman <sukhmansinghsaluja@gmail.com>
2025-04-22 14:14:30 +0530

    Add reference test

--- test/advanced/reference_test.c
+++ test/advanced/reference_test.c
@@ -1,28 +1,27 @@
-// void increment(int *a)
-// {
 
-//     breakpoint;
-//     breakpoint_global;
-
-//     (*a)++;
-
-//     breakpoint;
-//     breakpoint_global;
-// }
-
-
-//  Add function call be reference
-
-void test()
+class MyClass
 {
+public:
+    void sayHello()
+    {
+        int k = 0;
+    }
+
+    void callFunction()
+    {
+        int x = 0;
+    }
+};
+
+void increment(int* a) {
+    (*a)++;
 }
 
-int main()
-{
-    // int a = 5;
-    // increment(&a);
-    void (*fp)() = test;
+int main(){
+    MyClass myObj;
+    myObj.callFunction();
+    int a = 5;
+    increment(&a);
+    void (*fp)(int *) = increment;
     fp();
-    breakpoint;
-    breakpoint_global;
 }
